a87e4f012fbd5d5aebe06c876de241400ce9bb27
hamitmizrak <hamitmizrak@gmail.com>
2022-02-08 11:48:20 +0300

    TeacherEntity ==> validation

--- src/main/java/com/hamitmizrak/data/entity/TeacherEntity.java
+++ src/main/java/com/hamitmizrak/data/entity/TeacherEntity.java
@@ -1,13 +1,15 @@
 package com.hamitmizrak.data.entity;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.hibernate.annotations.CreationTimestamp;
-
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -19,6 +21,7 @@ import java.util.Date;
 
 @Entity
 @Table(name = "teacher")
+//@Valid @NotEmpty(message="")  @Email(message="") @Min(message"" value="")
 public class TeacherEntity implements Serializable {
 
     @Id
@@ -27,12 +30,13 @@ public class TeacherEntity implements Serializable {
     private Long teacherId;
 
     @Column(name = "teacher_name")
+    @NotEmpty(message = "adı alanını boş geçemezsiniz")
+    @Size(message = "en fazla 40 karakter girebilirsiniz",min = 1,max = 40)
     private String teacherName;
 
-    @Column(name = "teacher_surname")
-    private String teacherSurname;
-
     @Column(name = "teacher_price")
+    @Min(message = "4500 altında yazamazsınız",value = 4500)
+    @Max(message = "12000 üstünde yazamazsınız",value = 12000)
     private double teacherPrice;
 
     @Column(name = "created_date")
