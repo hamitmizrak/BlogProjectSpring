fa9f63f106d5d9dd6a7e5b708002ad494f75a793
hamitmizrak <hamitmizrak@gmail.com>
2022-02-08 16:59:43 +0300

    crud

--- src/main/java/com/hamitmizrak/ui/mvc/TeacherController.java
+++ src/main/java/com/hamitmizrak/ui/mvc/TeacherController.java
@@ -1,16 +1,13 @@
 package com.hamitmizrak.ui.mvc;
+
 import com.hamitmizrak.data.entity.TeacherEntity;
 import com.hamitmizrak.data.repository.TeacherRepository;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 
 @Controller
 @Log4j2
@@ -19,27 +16,14 @@ public class TeacherController {
     @Autowired
     TeacherRepository teacherRepository;
 
-    // http://localhost:8080/create/teacher
-    @GetMapping("/create/teacher")
-    public String createGetTeacher(Model model) {
-        TeacherEntity teacherEntity = TeacherEntity
-                .builder()
-                .teacherId(0L)
-                .teacherName("adınız")
-                .teacherPrice(0)
-                .build();
-        model.addAttribute("key_teacher", teacherEntity);
-        return "/teacherpage/teacher";
-    }
-
-    // http://localhost:8080/create/teacher
-    @PostMapping("/create/teacher")
-    public String createPostTeacher(@Valid @ModelAttribute("key_teacher") TeacherEntity teacherEntity, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "/teacherpage/teacher";
-        }
-        log.info(teacherEntity);
-        teacherRepository.save(teacherEntity);
-        return "success";
+    //DELETE
+    // http://localhost:8080/delete/teacher/1
+    @GetMapping("/delete/teacher/{id}")
+    public String deleteTeacher(@PathVariable(name = "id") Long id, Model model) {
+        model.addAttribute("silindi", "Teacher silindi");
+        teacherRepository.deleteById(id);
+        Iterable<TeacherEntity> teacherList = teacherRepository.findAll();
+        model.addAttribute("key_teacher_list", teacherList);
+        return "/teacherlist";
     }
 }
