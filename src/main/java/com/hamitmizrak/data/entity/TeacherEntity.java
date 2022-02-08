8c83e5eb71f0332805a6feee24347bcd4e1899b7
hamitmizrak <hamitmizrak@gmail.com>
2022-02-08 10:38:06 +0300

    @Entit ==> TeacherEntity ==> Lombok

--- src/main/java/com/hamitmizrak/data/entity/TeacherEntity.java
+++ src/main/java/com/hamitmizrak/data/entity/TeacherEntity.java
@@ -1,10 +1,21 @@
 package com.hamitmizrak.data.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Log4j2
 
 @Entity
 @Table(name = "teacher")
