c2a2c81c4c03f3969c3e2dd0b31833f68c876dd7
hamitmizrak <hamitmizrak@gmail.com>
2022-02-08 16:58:34 +0300

    revize

--- src/main/java/com/hamitmizrak/BlogProjectApplication.java
+++ src/main/java/com/hamitmizrak/BlogProjectApplication.java
@@ -3,10 +3,12 @@ package com.hamitmizrak;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BlogProjectApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BlogProjectApplication.class, args);
     }
 
