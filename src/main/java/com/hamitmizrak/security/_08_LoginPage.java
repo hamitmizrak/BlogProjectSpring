a8a2508f4588517ebcbb7d4f648670ff71de1ba5
hamitmizrak <hamitmizrak@gmail.com>
2022-02-10 22:39:11 +0300

    Sayfa Yetkilendirme

---
+++ src/main/java/com/hamitmizrak/security/_08_LoginPage.java
@@ -0,0 +1,40 @@
+package com.hamitmizrak.security;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class _08_LoginPage extends WebSecurityConfigurerAdapter {
+
+    //httpBasic Açmak istiyorsam bu metodu yazalım
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                    .authorizeRequests()
+                    .antMatchers("/","/index","/success","/security/public") //dikkat: Controler url olacak
+                    .permitAll()//her isteği izin ver
+                    .antMatchers("/login")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                //.csrf().disable()//guvenlik zafiyeti
+                .formLogin()
+                    .loginPage("/login")//dikkat: Controler url olacak
+                    .defaultSuccessUrl("/security/private") //dikkat: Controler url olacak
+                    .and()
+                .httpBasic(); //popup seklinde userName ve password acılmasına denir
+    }
+
+    // http://localhost:8080
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authentication) throws Exception {
+        authentication
+                .inMemoryAuthentication()
+                .withUser("hamitmizrak")
+                .password("{noop}root")
+                .roles("USER");
+    }
+}
