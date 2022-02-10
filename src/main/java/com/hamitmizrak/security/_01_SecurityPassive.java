035016de48d42e020f43a26a515e439b72b8636d
hamitmizrak <hamitmizrak@gmail.com>
2022-02-10 22:35:36 +0300

    SecurityPassive

---
+++ src/main/java/com/hamitmizrak/security/_01_SecurityPassive.java
@@ -0,0 +1,15 @@
+package com.hamitmizrak.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//diger classdaki calissin diye kapattım
+@EnableWebSecurity
+public class _01_SecurityPassive extends WebSecurityConfigurerAdapter {
+
+    //kapatmak istiyorsam bu metodu yazalım
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    }
+}
