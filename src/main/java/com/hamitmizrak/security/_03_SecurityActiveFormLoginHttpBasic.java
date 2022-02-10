653197438052dadc51555db5ba6d54fedc2007d7
hamitmizrak <hamitmizrak@gmail.com>
2022-02-10 22:36:14 +0300

    SecurityActiveFormLoginHttpBasic

---
+++ src/main/java/com/hamitmizrak/security/_03_SecurityActiveFormLoginHttpBasic.java
@@ -0,0 +1,22 @@
+package com.hamitmizrak.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class _03_SecurityActiveFormLoginHttpBasic extends WebSecurityConfigurerAdapter {
+
+    //httpBasic Açmak istiyorsam bu metodu yazalım
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .anyRequest()//her isteği izin ver
+                    .authenticated()
+                    .and()
+                .formLogin()
+                    .and()
+                .httpBasic(); //popup seklinde userName ve password acılmasına denir
+    }
+}
