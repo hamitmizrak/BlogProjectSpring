50b845ee8103fecc0064c65ad012e59c8968b812
hamitmizrak <hamitmizrak@gmail.com>
2022-02-10 22:35:58 +0300

    SecurityActiveHttpBasic

---
+++ src/main/java/com/hamitmizrak/security/_02_SecurityActiveHttpBasic.java
@@ -0,0 +1,22 @@
+package com.hamitmizrak.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//diger classdaki calissin diye kapattım
+@EnableWebSecurity
+public class _02_SecurityActiveHttpBasic extends WebSecurityConfigurerAdapter {
+
+
+    //httpBasic Açmak istiyorsam bu metodu yazalım
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .anyRequest()//her isteği izin ver
+                    .authenticated()
+                    .and()
+                .httpBasic(); //popup seklinde userName ve password acılmasına denir
+    }
+}
