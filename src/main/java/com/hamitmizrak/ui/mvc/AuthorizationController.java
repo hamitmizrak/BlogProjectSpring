5b0fdb44a4584ce1568700a4d9d194b656afdaff
hamitmizrak <hamitmizrak@gmail.com>
2022-02-11 22:57:25 +0300

    Authorization ( Bir sayfada  belirli kişileri yetkilendirmek)

--- src/main/java/com/hamitmizrak/ui/mvc/AuthorizationController.java
+++ src/main/java/com/hamitmizrak/ui/mvc/AuthorizationController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 //Belirli sayfalara belirli kişiler girebilsin(Authorization)
 public class AuthorizationController {
 
-    //süperadmin bütün sayfalara erişim vardır.
+    //admin bütün sayfalara erişim vardır.
     //writer sadece kendi sayfasına erişim vardır.
 
     // SUPER ADMIN
@@ -44,7 +44,6 @@ public class AuthorizationController {
         return "/writer/public";
     }
 
-
     // ROLES
     @GetMapping("/roles")
     @ResponseBody
@@ -59,4 +58,16 @@ public class AuthorizationController {
         return " Sistem Yetkilendirme kişileri: " + rols;
     }
 
+    // FarkliKullanicilar Aynı Sayfada Farkl kullanıcılar
+    // http://localhost:8080/spesific_page
+    @GetMapping("/spesific_page")
+    public String getSpesific(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String user = "";
+        if (authentication != null) {
+            user = authentication.getName();
+        }
+        model.addAttribute("system_user", user);
+        return "/aynisayfafarklikullanici/farklıkullanicilar";
+    }
 }
